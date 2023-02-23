01db9acee1a62c5f9c1294e9a7d748f11cb1292e
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-02-23 13:22:40 +0100

    feat[bitter_work]:

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,16 @@
-$(document).ready(
-  $('input').toggle(function () {
-    let data = [];
-    $('li').append();
-  })
-);
+$(document).ready(func1);
+
+function func1 () {
+  const amenitiesDict = {};
+  $('.amenities input').change(function () {
+    if ($(this).is(':checked')) {
+      amenitiesDict[$(this).attr('data-name')] = $(this).attr('data-id');
+    // console.log(amenitiesDict)
+    } else {
+      delete amenitiesDict[$(this).attr('data-name')];
+    // console.log(amenitiesDict)
+    }
+    const names = Object.keys(amenitiesDict);
+    $('.amenities > h4').text(names.sort().join(', '));
+  });
+}
