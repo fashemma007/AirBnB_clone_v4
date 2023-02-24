0c79c8a28cb7ada0f49a6b64a6d72959fbc2a2d4
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-02-24 08:28:48 +0100

    feat[places]: load places with jquery on the frontend

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -30,6 +30,13 @@ $.ajax({
   contentType: 'application/json; charset=utf-8',
   dataType: 'json',
   success: function (data) {
-    console.log(data);
+    // console.log(data)
+    for (const place of data) {
+      console.log(place);
+      // console.log(place.name)
+      $('.places').append(
+        '<article><div div class= "title_box" ><h2>' +
+        place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + place.number_bathrooms + ' Bathroom</div></div><div class="user"><div class="description">' + place.description + '</div></article>');
+    }
   }
 });
